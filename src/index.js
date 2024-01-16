e11badaf03c32c17285c2d52088dd01b336d611b
JessicaBoos <jessica.boos28@gmail.com>
2024-01-16 13:09:02 +0100

    updated default behaviour.

--- src/index.js
+++ src/index.js
@@ -51,3 +51,5 @@ function handleSearchSubmit(event) {
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
+
+searchCity("Los Angeles");
